f6b0318afb7b1fa7570907be550aff5f6e3f2bea
EkaterinaEl <hackerrussia@yandex.ru>
2023-06-07 11:56:34 +0500

    Create folders structure

--- app/src/main/java/com/elka/foody/presentation/MainActivity.kt
+++ app/src/main/java/com/elka/foody/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.elka.foody
+package com.elka.foody.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.elka.foody.R
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
